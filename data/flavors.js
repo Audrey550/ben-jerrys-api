02e7ab1b3269d90947a20ef0f043aa6641738c5e
Audrey <r0982547@student.thomasmore.be>
2026-08-19 00:56:23 +0200

    Move flavor data to separate file

---
+++ data/flavors.js
@@ -0,0 +1,12 @@
+const flavors = [
+  {
+    id: 1,
+    name: "Cookie Dough"
+  },
+  {
+    id: 2,
+    name: "Chocolate Fudge Brownie"
+  }
+];
+
+module.exports = flavors;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const flavors = require("./data/flavors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,26 @@ app.get("/", (req, res) => {
   res.json({
     message: "Ben & Jerry's API is running 🍦"
   });
+});
+
+app.get("/api/flavors", (req, res) => {
+  res.json({
+    flavors
+  });
+});
+
+app.get("/api/flavors/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const flavor = flavors.find((flavor) => flavor.id === id);
+
+  if (!flavor) {
+    return res.status(404).json({
+      message: "Flavor not found"
+    });
+  }
+
+  res.json(flavor);
 });
 
 app.listen(PORT, () => {
